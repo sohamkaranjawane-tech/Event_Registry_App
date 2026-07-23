157df6ee679344d3e0b78d6930f4d98e04e8e8bd
sohamkaranjawane-tech <sohamkaranjawane@gmail.com>
2026-07-23 22:01:17 +0530

    Update frontend

--- backen/index.js
+++ backen/index.js
@@ -12,7 +12,10 @@ const eventRoute = require('./routes/eventRoute');
 //middlewares
 app.use(express.json());
 app.use(cookieParser());
-app.use(cors({origin: "http://localhost:5173",credentials: true}));
+app.use(cors({
+    origin: "https://event-registry-app.vercel.app",
+    credentials: true
+}));
 
 //Usage of Routes
 app.use('/authRoute',authRoute);
